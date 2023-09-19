870142bfdb791e6631f41f1e96e258a4608b22b2
Avani <avani.jadeja@gmail.com>
2023-09-19 13:00:11 -0500

    Added name in navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,63 +3,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Nav() {
-
   function showNavigation() {
-   <ul>
-          <li className="mx-1">
-            <Link to="/About.js">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/Caterning.js">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/Contact.js">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/DeliveryApps.js">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/Gallery.js">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/Home.js">
-              Login
-            </Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/OnlineOrder.js">
-              Login
-            </Link>
-          </li>
-          
-
-        </ul>
-  
+    <ul>
+      <li className="mx-1">
+        <Link to="/About.js">About</Link>
+      </li>
+      <li className="mx-1">
+        <Link to="/Caterning.js">Catering</Link>
+      </li>
+      <li className="mx-1">
+        <Link to="/Contact.js">Contact</Link>
+      </li>
+      <li className="mx-1">
+        <Link to="/DeliveryApps.js">DeliveryApps</Link>
+      </li>
+      <li className="mx-1">
+        <Link to="/Gallery.js">Gallery</Link>
+      </li>
+      <li className="mx-1">
+        <Link to="/Home.js">Home</Link>
+      </li>
+      <li className="mx-1">
+        <Link to="/OnlineOrder.js">OnlineOrder</Link>
+      </li>
+    </ul>;
   }
 
   return (
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-         <h1>Rajvadu</h1>
-    
+          <h1>Rajvadu</h1>
         </Link>
       </h1>
 
-      <nav>
-     
-        {/* {showNavigation()} */}
-      </nav>
+      <nav>{/* {showNavigation()} */}</nav>
     </header>
   );
 }
