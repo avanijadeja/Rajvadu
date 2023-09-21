8adb2cbe781cf5cc0c5293e8d5f98cccabfb58f6
Avani <avani.jadeja@gmail.com>
2023-09-21 15:07:06 -0500

    Restructure folders

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { AiFillLayout } from "react-icons/ai";
 import food1 from "../assets/images/food1.jpeg";
+import "./Gallery.css";
 
 const Gallery = () => {
   return (
-    <div className="Gallery">
-      <img src ={food1} alt="food1" />
-      <h1 id="gallery">Gallery</h1>
-    </div>
+    <>
+      <div className="Gallery">
+        <img src={food1} alt="food1" />
+        <div>
+          <h1 id="gallery">Gallery</h1>
+        </div>
+      </div>
+      <div>
+        <h1> Gallery</h1>
+        <h1> Gallery</h1> <h1> Gallery</h1> <h1> Gallery</h1> <h1> Gallery</h1>{" "}
+        <h1> Gallery</h1> <h1> Gallery</h1> <h1> Gallery</h1> <h1> Gallery</h1>{" "}
+        <h1> Gallery</h1> <h1> Gallery</h1> <h1> Gallery</h1> <h1> Gallery</h1>{" "}
+        <h1> Gallery</h1>
+      </div>
+    </>
   );
 };
 
