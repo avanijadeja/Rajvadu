6b110ad2ba80d280a9bed79bd6d918f32e28b229
Avani <avani.jadeja@gmail.com>
2023-09-22 11:09:13 -0500

    Added gallery list in helpers folder

---
+++ client/src/helpers/GalleryList.js
@@ -0,0 +1,43 @@
+import food1 from "../assets/food1.jpg";
+import food2 from "../assets/food2.jpg";
+import food3 from "../assets/food3.jpg";
+import food4 from "../assets/food4.jpg";
+import food5 from "../assets/food5.jpg";
+import food6 from "../assets/food6.jpg";
+import food7 from "../assets/food7.jpg";
+import food8 from "../assets/food8.jpg";
+import food9 from "../assets/food9.jpg";
+import food10 from "../assets/food10.jpg";
+
+export const MenuList = [
+  {
+    image: food1,
+  },
+  {
+    image: food2,
+  },
+  {
+    image: food3,
+  },
+  {
+    image: food4,
+  },
+  {
+    image: food5,
+  },
+  {
+    image: food6,
+  },
+  {
+    image: food7,
+  },
+  {
+    image: food8,
+  },
+  {
+    image: food9,
+  },
+  {
+    image: food10,
+  },
+];
