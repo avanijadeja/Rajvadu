19be8369693073d698bca609cf95f5cd9819f4e2
Avani <avani.jadeja@gmail.com>
2023-09-22 11:23:23 -0500

    Change image name

--- client/src/helpers/GalleryList.js
+++ client/src/helpers/GalleryList.js
@@ -1,15 +1,15 @@
-import food1 from "../assets/food1.jpg";
-import food2 from "../assets/food2.jpg";
-import food3 from "../assets/food3.jpg";
-import food4 from "../assets/food4.jpg";
-import food5 from "../assets/food5.jpg";
-import food6 from "../assets/food6.jpg";
-import food7 from "../assets/food7.jpg";
-import food8 from "../assets/food8.jpg";
-import food9 from "../assets/food9.jpg";
-import food10 from "../assets/food10.jpg";
+import food1 from "../assets/food1.jpeg";
+import food2 from "../assets/food2.jpeg";
+import food3 from "../assets/food3.jpeg";
+import food4 from "../assets/food4.jpeg";
+import food5 from "../assets/food5.jpeg";
+import food6 from "../assets/food6.jpeg";
+import food7 from "../assets/food7.jpeg";
+import food8 from "../assets/food8.jpeg";
+import food9 from "../assets/food9.jpeg";
+import food10 from "../assets/food10.jpeg";
 
-export const MenuList = [
+export const GalleryList = [
   {
     image: food1,
   },
