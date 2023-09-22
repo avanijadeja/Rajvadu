1033fb5601561b743f7af0de02984f739ee1593c
Avani <avani.jadeja@gmail.com>
2023-09-22 15:19:20 -0500

    Added Deliveryapps.js page

--- client/src/pages/DeliveryApps.js
+++ client/src/pages/DeliveryApps.js
@@ -1,10 +1,34 @@
 import React from "react";
 
-const DeliveryApps= () => {
+import doordash from "../assets/doordash.jpeg";
+import grubhub from "../assets/grubhub.jpeg";
+import seamless from "../assets/seamless.jpeg";
+import ubereats from "../assets/ubereats.jpeg";
+
+const DeliveryApps = () => {
   return (
-    <div>
-      <h1>DeliveryApps</h1>
-      
+    <div className="delivery">
+      <div
+        className="backgroundTop"
+        style={{ backgroundImage: `url(${food2})` }}
+      >
+        <div className="backgroundHeader">
+          <h3>We are in</h3>
+          <h1>Our Delivery Apps</h1>
+          <div class="icon">
+            <a href="#aboutimages">
+              <FaCircleChevronDown size={70} color="white" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="deliveryimages">
+        <img src={doordash} alt="imagemain"></img>
+        <img src={grubhub} alt="imagemain"></img>
+        <img src={seamless} alt="imagemain"></img>
+        <img src={ubereats} alt="imagemain"></img>
+      </div>
     </div>
   );
 };
