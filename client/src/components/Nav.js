e92fc8cb89792f9aba3b0c75e9d0cafc457c3d28
Avani <avani.jadeja@gmail.com>
2023-09-25 14:15:56 -0500

    Added navbar design logic

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,38 +1,111 @@
 import React from "react";
-// import Auth from "../../utils/auth";
+import Auth from "../../utils/auth";
 import "../styles/Nav.css";
 import { Link } from "react-router-dom";
 
 function Nav() {
   function showNavigation() {
-    return (
-      <div class="Navbarcss">
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/">HOME</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/About">ABOUT</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/Gallery">GALLERY</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/OnlineOrder">ONLINE ORDER</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/DeliveryApps">DELIVERY APPS</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/Catering">CATERNING</Link>
-          </li>
-          <li className="mx-1">
-            <Link to="/Contact">CONTACT</Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+    if (Auth.loggedIn()) {
+      return (
+        <div class="Navbarcss">
+          <ul className="flex-row">
+            <li className="mx-1">
+              <Link to="/">HOME</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/About">ABOUT</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/Gallery">GALLERY</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/OnlineOrder">ONLINE ORDER</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/DeliveryApps">DELIVERY APPS</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/Catering">CATERNING</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/Contact">CONTACT</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/orderHistory">Order History</Link>
+            </li>
+            <li className="mx-1">
+              {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+              <a href="/" onClick={() => Auth.logout()}>
+                Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <div class="Navbarcss">
+          <ul className="flex-row">
+            <li className="mx-1">
+              <Link to="/">HOME</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/About">ABOUT</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/Gallery">GALLERY</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/OnlineOrder">ONLINE ORDER</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/DeliveryApps">DELIVERY APPS</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/Catering">CATERNING</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/Contact">CONTACT</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/signup">Signup</Link>
+            </li>
+            <li className="mx-1">
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
+  //   return (
+  //     <div class="Navbarcss">
+  //       <ul className="flex-row">
+  //         <li className="mx-1">
+  //           <Link to="/">HOME</Link>
+  //         </li>
+  //         <li className="mx-1">
+  //           <Link to="/About">ABOUT</Link>
+  //         </li>
+  //         <li className="mx-1">
+  //           <Link to="/Gallery">GALLERY</Link>
+  //         </li>
+  //         <li className="mx-1">
+  //           <Link to="/OnlineOrder">ONLINE ORDER</Link>
+  //         </li>
+  //         <li className="mx-1">
+  //           <Link to="/DeliveryApps">DELIVERY APPS</Link>
+  //         </li>
+  //         <li className="mx-1">
+  //           <Link to="/Catering">CATERNING</Link>
+  //         </li>
+  //         <li className="mx-1">
+  //           <Link to="/Contact">CONTACT</Link>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //   );
+  // }
 
   return (
     <>
