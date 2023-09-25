05e7c02042bbf7c91e323e0f93340c8b6a82e99e
Avani <avani.jadeja@gmail.com>
2023-09-25 13:34:04 -0500

    Added connection logic in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   ApolloProvider,
-//   createHttpLink,
-// } from "@apollo/client";
-// import { setContext } from "@apollo/client/link/context";
-// import { Provider } from "react-redux";
-// import store from "./utils/store"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
@@ -23,52 +23,54 @@ import DeliveryApps from "./pages/DeliveryApps";
 import Catering from "./pages/Catering";
 import Contact from "./pages/Contact";
 
-// const httpLink = createHttpLink({
-//   uri: "/graphql",
-// });
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
 
-// const authLink = setContext((_, { headers }) => {
-//   const token = localStorage.getItem("id_token");
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : "",
-//     },
-//   };
-// });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    // <ApolloProvider client={client}>
-    <Router>
-      {/* <Provider store={store}> */}
-      <div className="page-container">
-        <div className="content-wrap">
-          <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/About" element={<About />} />
-            <Route path="/Gallery" element={<Gallery />} />
-            <Route path="/Gallery" element={<Gallery />} />
-            <Route path="/OnlineOrder" element={<OnlineOrder />} />
-            <Route path="/DeliveryApps" element={<DeliveryApps />} />
-            <Route path="/Catering" element={<Catering />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="*" element={<NoMatch />} />
-          </Routes>
-          {/* </Provider> */}
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Provider store={store}>
+            <div className="page-container">
+              <div className="content-wrap">
+                <Nav />
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/About" element={<About />} />
+                  <Route path="/Gallery" element={<Gallery />} />
+                  <Route path="/Gallery" element={<Gallery />} />
+                  <Route path="/OnlineOrder" element={<OnlineOrder />} />
+                  <Route path="/DeliveryApps" element={<DeliveryApps />} />
+                  <Route path="/Catering" element={<Catering />} />
+                  <Route path="/Contact" element={<Contact />} />
+                  <Route path="*" element={<NoMatch />} />
+                </Routes>
+              </div>
+            </div>
+          </Provider>
+
+          <Footer />
         </div>
-
-        <Footer />
-      </div>
-    </Router>
-
-    // </ApolloProvider>
+      </Router>
+      //{" "}
+    </ApolloProvider>
   );
 }
 
