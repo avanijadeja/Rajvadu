99ff59e33e906dc0fd9390645dda563a99698be7
Avani <avani.jadeja@gmail.com>
2023-09-25 09:36:54 -0500

    Added input validaition logic in contact.js

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -6,6 +6,47 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/Contact.css";
 
 const Contact = () => {
+  // define useState form name, email, message ,set default value as empty string
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  // Define useState for errorMessage, default value empty string
+  const [errorMessage, setErrorMessage] = useState("");
+
+  // destructure formState
+  const { name, email, message } = formState;
+
+  // define handleChange function for if field empty and not valid, Errormessage is display
+  function handleChange(e) {
+    //  check for email , if email has invalid format or blank then pass error message.
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("please enter a valid email");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  // preventDefault function on submit
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="contact">
       <div
